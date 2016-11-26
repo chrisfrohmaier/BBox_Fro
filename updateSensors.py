23cf22a344cfb6661eed68c75d30e57c27106fdc
chrisfrohmaier <cf5g09@soton.ac.uk>
2016-11-26 17:47:40 +0000

    Script to add data to sensors table in the mysql database

--- updateSensors.py
+++ updateSensors.py
@@ -11,4 +11,4 @@ import _mysql
 
 db = _mysql.connect(host='localhost', user='pi', passwd='raspberry', db='bbox')
 
-cur.execute("INSERT INTO sensors(irtemp,dietemp,voltage,ambtemp,pressure) VALUES(%s,%s,%s,%s,%s)",(float(tmp.readObjTempC()),float(tmp.readDieTempC()),float(tmp.readVoltage()),float(bmp.read_temperature()),float(bmp.read_pressure()),float(bmp.read_altitude())))
+db.execute("INSERT INTO sensors(irtemp,dietemp,voltage,ambtemp,pressure) VALUES(%s,%s,%s,%s,%s)",(float(tmp.readObjTempC()),float(tmp.readDieTempC()),float(tmp.readVoltage()),float(bmp.read_temperature()),float(bmp.read_pressure()),float(bmp.read_altitude())))
