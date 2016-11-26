bbd0d79ef1cb49ac0946fcbaae5ad9396c961ee0
chrisfrohmaier <cf5g09@soton.ac.uk>
2016-11-26 17:44:38 +0000

    Script to add data to sensors table in the mysql database

---
+++ updateSensors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+__author__ = "Chris Frohmaier"
+__email__ = "c.frohmaier2soton.ac.uk"
+__status__ = "In development"
+__date__ = "November 2016"
+
+import RPi.GPIO as GPIO # always needed with RPi.GPIO
+from BMP085 import BMP085
+from TMP007 import TMP007
+import _mysql
+
+con = _mysql.connect(host='localhost', user='pi', passwd'test623', db='bbox')
+cur = con.cursor()
+
+cur.execute("INSERT INTO sensors(irtemp,dietemp,voltage,ambtemp,pressure) VALUES(%s,%s,%s,%s,%s)",(float(tmp.readObjTempC()),float(tmp.readDieTempC()),float(tmp.readVoltage()),float(bmp.read_temperature()),float(bmp.read_pressure()),float(bmp.read_altitude())))
